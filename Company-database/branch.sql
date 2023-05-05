252f7a549db6e49630f84dcd37e0bf6fb7e40f81
HassanMunene <awanzihassan@gmail.com>
2023-05-05 20:15:18 +0300

    adding the reference table

--- Company-database/branch.sql
+++ Company-database/branch.sql
@@ -1,10 +1,13 @@
 -- This script will create the a table called branch
 -- it will have the following fields
 -- branch_id, branch_name
+-- we will also have manager_id that is a foreign key that reference the emp_id -- of the Employee table
 
 
 DROP TABLE branch;
 CREATE TABLE branch(
 	branch_id INT PRIMARY KEY,
-	branch_name VARCHAR(50)
+	branch_name VARCHAR(50),
+	manager_id INT,
+	FOREIGN KEY(manager_id) REFERENCES Employee(emp__id)
 );
