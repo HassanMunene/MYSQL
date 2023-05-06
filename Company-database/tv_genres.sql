5edbf1c2756ec390f5de0b02b31da7221114c79a
HassanMunene <awanzihassan@gmail.com>
2023-05-06 21:20:32 +0300

    more corrections

--- Company-database/tv_genres.sql
+++ Company-database/tv_genres.sql
@@ -17,8 +17,8 @@ UNLOCK TABLES;
 -- another FK connects with the shows
 -- so basically we are connecting the shows and the genres
 
-drop table if exists tv_show_genres;
-create table tv_show_genres (
+DROP TABLE IF EXISTS tv_show_genres;
+CREATE TABLE IF EXISTS tv_show_genres (
 	show_id int(11) NOT NULL,
 	genre_id int(11) NOT NULL,
 	FOREIGN KEY (show_id) REFERENCES tv_shows(id),
@@ -27,14 +27,14 @@ create table tv_show_genres (
 
 -- let now populate the table with data
 
-lock tables tv_show_genres write;
-insert into tv_show_genres values (2,1), (2,2),(3,3),(3,1),(3,4),(4,5),(5,5),(6,5),(7,6),(7,1),(7,7),(7,8),(9,6),(9,1),(9,2),(9,7),(9,8),(11,5),(11,1);
-unlock tables;
+LOCK TABLES tv_show_genres WRITE;
+INSERT INTO tv_show_genres VALUES (2,1), (2,2),(3,3),(3,1),(3,4),(4,5),(5,5),(6,5),(7,6),(7,1),(7,7),(7,8),(9,6),(9,1),(9,2),(9,7),(9,8),(11,5),(11,1);
+UNLOCK TABLES;
 
 -- table structure fro tv_shows
 
-drop table if exists tv_shows;
-create table tv_shows (
+DROP TABLE IF EXISTS tv_shows;
+CREATE TABLE tv_shows (
 	id int(11) not null auto_increment,
 	title varchar(256) NOT NULL,
 	PRIMARY KEY(id)
@@ -42,6 +42,6 @@ create table tv_shows (
 
 -- populate the table tv_shows now
 
-lock tables tv_shows write;
-insert into tv_shows values (2,'House'),(3,'Game of Thrones'),(4,'The Big Bang Theory'),(5,'New Girl'),(6,'Silicon Valley'),(7,'Breaking Bad'),(8,'Better Call Saul'),(9,'Dexter'),(10,'Homeland'),(11,'The Last Man on Earth');
-unlock tables;
+LOCK TABLES tv_shows write;
+INSERT INTO tv_shows VALUES (2,'House'),(3,'Game of Thrones'),(4,'The Big Bang Theory'),(5,'New Girl'),(6,'Silicon Valley'),(7,'Breaking Bad'),(8,'Better Call Saul'),(9,'Dexter'),(10,'Homeland'),(11,'The Last Man on Earth');
+UNLOCK TABLES;
